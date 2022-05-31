8a79b9ffc4b3e255e4e923c5ef63cd9c0eb4e1c2
Adewale Samuel <samuel.adewale@pglnigeria.com>
2022-05-31 17:38:16 +0100

    initial commit

---
+++ framework/storage/framework/views/5ad59f86bd6b64867f3d467db0b290402de8b7d4.php
@@ -0,0 +1,105 @@
+
+<?php $__env->startSection("breadcrumb"); ?>
+<li class="breadcrumb-item"><a href="<?php echo e(route("mechanic.index")); ?>"> <?php echo app('translator')->get('fleet.mechanics'); ?> </a></li>
+<li class="breadcrumb-item active"><?php echo app('translator')->get('fleet.add_mechanic'); ?></li>
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('content'); ?>
+<div class="row">
+  <div class="col-md-12">
+    <div class="card card-success">
+      <div class="card-header">
+        <h3 class="card-title"><?php echo app('translator')->get('fleet.create_mechanic'); ?></h3>
+      </div>
+
+      <div class="card-body">
+        <?php if(count($errors) > 0): ?>
+          <div class="alert alert-danger">
+            <ul>
+              <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+              <li><?php echo e($error); ?></li>
+              <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </ul>
+          </div>
+        <?php endif; ?>
+
+        <?php echo Form::open(['route' => 'mechanic.store','method'=>'post']); ?>
+
+        <?php echo Form::hidden('user_id',Auth::user()->id); ?>
+
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <?php echo Form::label('name',__('fleet.name'), ['class' => 'form-label']); ?>
+
+              <?php echo Form::text('name',null,['class'=>'form-control','required']); ?>
+
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="form-group">
+              <?php echo Form::label('email',__('fleet.email'), ['class' => 'form-label']); ?>
+
+              <div class="input-group mb-3">
+              <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fa fa-envelope"></i></span></div>
+              <?php echo Form::email('email',null,['class'=>'form-control','required']); ?>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="form-group">
+              <?php echo Form::label('contact_number',__('fleet.contact_number'), ['class' => 'form-label']); ?>
+
+              <div class="input-group mb-3">
+              <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fa fa-phone"></i></span></div>
+              <?php echo Form::number('contact_number',null,['class'=>'form-control','required']); ?>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="form-group">
+              <?php echo Form::label('category',__('fleet.category'), ['class' => 'form-label']); ?>
+
+              
+              <?php echo Form::text('category',null,['class'=>'form-control','required']); ?>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col-md-12 m-3">
+            <?php echo Form::submit(__('fleet.add_mechanic'), ['class' => 'btn btn-success']); ?>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection("script"); ?>
+<script type="text/javascript">
+  $(".add_udf").click(function () {
+    // alert($('#udf').val());
+    var field = $('#udf1').val();
+    if(field == "" || field == null){
+      alert('Enter field name');
+    }
+    else{
+      $(".blank").append('<div class="row"><div class="col-md-8">  <div class="form-group"> <label class="form-label">'+ field.toUpperCase() +'</label> <input type="text" name="udf['+ field +']" class="form-control" placeholder="Enter '+ field +'" required></div></div><div class="col-md-4"> <div class="form-group" style="margin-top: 30px"><button class="btn btn-danger" type="button" onclick="this.parentElement.parentElement.parentElement.remove();">Remove</button> </div></div></div>');
+      $('#udf1').val("");
+    }
+  });
+</script>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\fleet-manager\framework\resources\views/mechanics/create.blade.php ENDPATH**/ ?>
